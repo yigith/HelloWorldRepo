e391cb6f4d6a6585d54b2b94fbf26746d169be43
canberksahin <61060444+canberksahin@users.noreply.github.com>
2020-02-14 21:09:27 +0300

    Türkçe eklendi

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Merhaba Dünya");
             Console.ReadKey();
         }
     }
